6d0753f4cf46b53c501be9ec5ea1039374c86372
Fikret Alan <115360494+fikretalan13@users.noreply.github.com>
2024-09-03 19:11:09 +0300

    Day 23
    Final level added.

---
+++ Lvl100platformgame/Assets/Scripts/Ending.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    [SerializeField] GameObject endingAnim;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player.instance.playerSpeed=0;
+            endingAnim.SetActive(true);
+        }
+    }
+}
